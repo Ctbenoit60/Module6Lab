b4f5b98831d76107ba0e777548691ecfd5dd7560
Ctbenoit60 <ctbenoit60@gmail.com>
2024-01-02 20:01:09 -0600

    added the Emoji.jsx

--- module6Lab/src/App.jsx
+++ module6Lab/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Greeting from './components/Greeting'
-
+import BigCats from './components/BigCat'
+import Emoji from './components/Emoji'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,10 @@ function App() {
   return (
     <>
       <Greeting/>
+      <br />
+      <br />
+      <BigCats/> 
+      <Emoji/>
     </>
   )
 }
